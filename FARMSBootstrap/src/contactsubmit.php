b5932886ac992b4db9c9794380afa5b215783711
Christopher Eby <ceby@geodecisions.com>
2017-11-02 22:47:54 -0400

    Updating to use new mail server. Adding newsletter and recaptcha lib.

--- FARMSBootstrap/src/contactsubmit.php
+++ FARMSBootstrap/src/contactsubmit.php
@@ -60,7 +60,8 @@
         $mail->Debugoutput = 'html';
 
         //Set the hostname of the mail server
-        $mail->Host = "SMTP.FARMSINTERNATIONAL.COM";
+        //$mail->Host = "SMTP.FARMSINTERNATIONAL.COM";
+        $mail->Host = "sub5.mail.dreamhost.com";
         //Set the SMTP port number - likely to be 25, 465 or 587
         $mail->Port = 587;
         //Whether to use SMTP authentication
